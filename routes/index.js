f8908925b0c62cec242b4221dd217aa476c2f3b5
Andrik Pineda Castillo <kean5817@gmail.com>
2020-08-25 18:30:07 -0500

    cambios en las rutas

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,18 @@
 const express = require('express');
 
-const route = express.Router();
+const router = express.Router();
+const foodController = require('../controller/foodController');
 
 
-route.get('/', (req, res) => { 
+
+router.get('/', (req, res) => { 
     res.send('Hola Mundo');
 });
 
+router.get('/food/',foodController.listFood);
+router.post('/food/', foodController.addFood);
+router.get('/food/:id', foodController.viewFood);
+router.put('/food/:id', foodController.updateFood);
+router.delete('/food/:id', foodController.deleteFood);
 
-module.exports = route;
+module.exports = router;
