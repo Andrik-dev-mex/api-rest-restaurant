0a08be21f5233c1a7e1649ae6acd70e044e878b4
Andrik Pineda Castillo <kean5817@gmail.com>
2020-08-25 18:25:19 -0500

    Cambios en las rutas

--- controller/foodController.js
+++ controller/foodController.js
@@ -1,14 +1,52 @@
 const CtrlFood = {};
-const Food = require('./models/food.js');
+const Food = require("../models/food.js");
 
-CtrlFood.addFood = async (req,res,next) => { 
+CtrlFood.addFood = async (req, res, next) => {
   try {
     const food = new Food(req.body);
-    food.save();
-    res.status(200).json({message : 'Nueva comida agregada'});
+    await food.save();
+    res.status(200).json({ message: "Nueva comida agregada" });
   } catch (error) {
     res.status(400).json(error);
   }
+};
+
+CtrlFood.listFood = async (req, res, next) => {
+  try {
+    const foods = await Food.find({});
+    res.status(200).json(foods);
+  } catch (error) {
+    res.status(400).json({ message: "Error al obtener los datos" });
+  }
+};
+
+CtrlFood.viewFood = async (req, res, next) => {
+  try {
+    const food = await Food.findById({ _id: req.params.id });
+    res.status(200).json(food);
+  } catch (error) {
+    res.status(404).json({ message: "no existe esa comida" });
+  }
+};
+
+CtrlFood.deleteFood = async (req, res, next) => {
+  try {
+    const food = await Food.findAndDeleteById({ _id: req.params.id });
+    res.status(200).json({ message: "Comida eliminada con exito" });
+  } catch (error) {
+    res.status(400).json({ message: "No se pudo eliminar esta comida" });
+  }
+};
+
+CtrlFood.updateFood = async (req, res, next) => {
+  try {
+    const food = await Food.findByIdAndUpdate(
+      { _id: req.params.id },
+      req.body,
+      { new: true }
+    );
+    res.status(200).json(food);
+  } catch (error) {}
 };
 
 module.exports = CtrlFood;
